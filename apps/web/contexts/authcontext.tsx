fb38a8c89f76811918feda3373752c8fa64c0c5f
stonecircuit <hellostonecircuit@gmail.com>
2026-06-19 10:04:48 +0530

    Create authentication foundation

---
+++ apps/web/contexts/authcontext.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function AuthContext() {
+  return null;
+}
---
+++ apps/web/services/authservice.ts
@@ -0,0 +1,11 @@
+export async function signUp() {
+  throw new Error("Not implemented");
+}
+
+export async function login() {
+  throw new Error("Not implemented");
+}
+
+export async function logout() {
+  throw new Error("Not implemented");
+}
